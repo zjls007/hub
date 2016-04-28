8677beab9ae87ea91d2b75b1a11508ca3dc97286
zjls007 <zjls007@qq.com>
2016-04-28 15:22:26 +0800

    nolog

--- src/main/java/com/demo/core/Config.java
+++ src/main/java/com/demo/core/Config.java
@@ -5,22 +5,22 @@ import java.util.ResourceBundle;
 
 public class Config {
 
-	public static String JDBC_URL = "jdbc.url";
-	public static String JDBC_USERNAME = "jdbc.username";
-	public static String JDBC_PASSWORD = "jdbc.password";
-	public static String HIBERNATE_DIALECT = "hibernate.dialect";
-	public static String HIBERNATE_SHOWSQL = "hibernate.show_sql";
-	public static String HIBERNATE_HBMDDL_AUTO = "hibernate.hbm2ddl.auto";
+	public static final String JDBC_URL;
+	public static final String JDBC_USERNAME;
+	public static final String JDBC_PASSWORD;
+	public static final String HIBERNATE_DIALECT;
+	public static final String HIBERNATE_SHOWSQL;
+	public static final String HIBERNATE_HBMDDL_AUTO;
     
 	static {
 		ResourceBundle rb = PropertyResourceBundle.getBundle("config");
 		
-    	JDBC_URL = rb.getString(JDBC_URL);
-    	JDBC_USERNAME = rb.getString(JDBC_USERNAME);
-    	JDBC_PASSWORD = rb.getString(JDBC_PASSWORD);
-    	HIBERNATE_DIALECT = rb.getString(HIBERNATE_DIALECT);
-    	HIBERNATE_SHOWSQL = rb.getString(HIBERNATE_SHOWSQL);
-    	HIBERNATE_HBMDDL_AUTO = rb.getString(HIBERNATE_HBMDDL_AUTO);
+    	JDBC_URL = rb.getString("jdbc.url");
+    	JDBC_USERNAME = rb.getString("jdbc.username");
+    	JDBC_PASSWORD = rb.getString("jdbc.password");
+    	HIBERNATE_DIALECT = rb.getString("hibernate.dialect");
+    	HIBERNATE_SHOWSQL = rb.getString("hibernate.show_sql");
+    	HIBERNATE_HBMDDL_AUTO = rb.getString("hibernate.hbm2ddl.auto");
 	}
 	
 }
